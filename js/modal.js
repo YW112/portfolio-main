4449cbf99b56dccc5d59339f6169c9e2605239cc
Julia <yuliawolf112@gmail.com>
2026-01-29 22:50:45 +0300

    botn tabs buttons open modal window

--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,13 @@
 const modalButton = document.querySelector('.more');
+const modalButtons = document.querySelectorAll('.more');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const closeButton = document.querySelector('.modal__close');
-modalButton.addEventListener('click', () => {
-    modal.classList.remove('hidden');
+
+modalButtons.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        modal.classList.remove('hidden');
+        })
 })
 overlay.addEventListener('click', () => {
     modal.classList.add('hidden');
