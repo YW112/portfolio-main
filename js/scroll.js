89257f1358cb85309b8a063fce0764a9e81e0040
Julia <yuliawolf112@gmail.com>
2026-01-31 00:29:38 +0300

    scroll

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,8 @@
 const links = document.querySelectorAll('.menu-list__link');
 const btn = document.querySelector('.main__button');
+const mainScroll = document.querySelector('.main__scroll');
 
-const allLinks = [...links, btn];
+const allLinks = [...links, btn, mainScroll];
 
 allLinks.forEach(link => {
     link.addEventListener('click', (event) => {
